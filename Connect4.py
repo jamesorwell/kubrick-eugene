b17e063eee0d47cd7c565d1768b8a43e7e36dfa2
Rory Holmes <roryholmes@kubrickgroup.com>
2020-07-03 13:01:08 +0100

    Added player1 and player2 methods

--- Connect4.py
+++ Connect4.py
@@ -6,10 +6,30 @@ class Connect4():
         self.player2 = player2
         self.boardsize = None
 
-    def board(self, width, height):
-        size = np.zeros(width*height).reshape(width,height)
-        print (size)
+
+
+    def boards(self, width, height):
+        size = np.zeros(width*height).reshape(height,width )
+        self.boardsize = size
+        self.columncounter = [0] * width
+        print (self.columncounter)
+        print (self.boardsize)
+
+
+    def player1turn(self):
+        x = input("Player one, choose your position(a-g)")
+        dict = {'a' : 1, 'b' : 2, 'c' : 3, 'd' : 4, 'e' : 5, 'f' : 6, 'g' : 7}
+        self.boardsize[]
+
+    def player2turn(self):
+        x = input("Player one, choose your position(a-g)")
+        dict = {'a' : 1, 'b' : 2, 'c' : 3, 'd' : 4, 'e' : 5, 'f' : 6, 'g' : 7}
+
+
+
+
 
 
 game = Connect4('teamEugene', 'James')
-game.board(5,7)
+game.boards(7,6)
+game.player1turn()
