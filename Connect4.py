0970f73ad354d2d1293746738976106d97d83a9c
Rory Holmes <roryholmes@kubrickgroup.com>
2020-07-03 15:40:21 +0100

    Game functionality in place, need to add win conditions

--- Connect4.py
+++ Connect4.py
@@ -1,29 +1,43 @@
 import numpy as np
 
 class Connect4():
-    def __init__(self, player1, player2):
+    def __init__(self, player1, player2, width = 7, height = 6):
         self.player1 = player1
         self.player2 = player2
-        self.boardsize = None
+        self.board = np.zeros(width*height).reshape(height,width )
+        self.width = width  - 1
+        self.height = height - 1
+        self.counter = [0] * 7
+        print (self.counter)
+        print (self.board)
 
-
-
-    def boards(self, width, height):
-        size = np.zeros(width*height).reshape(height,width )
-        self.boardsize = size
-        self.columncounter = [0] * width
-        print (self.columncounter)
-        print (self.boardsize)
 
 
     def player1turn(self):
         x = input("Player one, choose your position(a-g)")
-        dict = {'a' : 1, 'b' : 2, 'c' : 3, 'd' : 4, 'e' : 5, 'f' : 6, 'g' : 7}
-        self.boardsize[]
+        dict = {'a' : 0, 'b' : 1, 'c' : 2, 'd' : 3, 'e' : 4, 'f' : 5, 'g' : 6}
+        self.board[self.height - self.counter[dict[x]]][dict[x]] = 1
+        self.counter[dict[x]] += 1
+        print (self.counter)
+        print (self.board)
+
+
 
     def player2turn(self):
-        x = input("Player one, choose your position(a-g)")
-        dict = {'a' : 1, 'b' : 2, 'c' : 3, 'd' : 4, 'e' : 5, 'f' : 6, 'g' : 7}
+        x = input("Player two, choose your position(a-g)")
+        dict = {'a' : 0, 'b' : 1, 'c' : 2, 'd' : 3, 'e' : 4, 'f' : 5, 'g' : 6}
+        self.board[self.height - self.counter[dict[x]]][dict[x]] = 2
+        self.counter[dict[x]] += 1
+        print (self.board)
+
+    def play(self):
+        turncount = 0
+        while turncount <= 5:
+            if turncount % 2 == 0:
+                game.player1turn()
+            else:
+                game.player2turn()
+            turncount+=1
 
 
 
@@ -31,5 +45,4 @@ class Connect4():
 
 
 game = Connect4('teamEugene', 'James')
-game.boards(7,6)
-game.player1turn()
+game.play()
